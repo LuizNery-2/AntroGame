52e7f887dbc6d6cfaa84ebf9dad99902f197ba90
LuizNery-2 <62023495+LuizNery-2@users.noreply.github.com>
2021-06-14 00:49:12 -0300

    Arrumando as animações

--- Projetinho/Assets/_scripts/player.cs
+++ Projetinho/Assets/_scripts/player.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class player : MonoBehaviour
     Vector3 velocidadeH;
     Vector3 velocidadeV;
     Vector3 velocidadefinal;
+    private Vector3 facingRight;
+    private Vector3 facingLeft;
     Animator animator;
     public float speed;
     public float alturaMax;
@@ -16,6 +19,9 @@ public class player : MonoBehaviour
     float gravidade;
     void Start()
     {
+        facingRight = transform.localScale;
+        facingLeft = transform.localScale;
+        facingLeft.x = facingRight.x * -1;
         animator = GetComponent<Animator>();
         gravidade = 2 * alturaMax/ Mathf.Pow(tempo, 2); 
         controller = GetComponent<CharacterController>();
@@ -24,26 +30,41 @@ public class player : MonoBehaviour
 
 
     private void FixedUpdate() {
-        if(Input.GetAxis("Horizontal") < 0){
-            animator.SetBool("esquerda", true);
+        Movimento();
+
+    }
+
+    private void Movimento(){
+        
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            animator.SetBool("Correr", true);
         }
-        else if(Input.GetAxis("Horizontal") > 0){
-            animator.SetBool("direita", true);
+        else
+        {
+            animator.SetBool("Correr", false);
         }
-        else if(Input.GetAxis("Horizontal") < 0.02 && Input.GetAxis("Horizontal") > -0.02 ){
-            animator.SetBool("direita", false);
-            animator.SetBool("esquerda", false);
+
+
+        if(Input.GetAxis("Horizontal") > 0){
+            transform.localScale = facingRight;
+            
         }
+        else if(Input.GetAxis("Horizontal") < 0){
+            transform.localScale = facingLeft;
+        }
+     
         if(controller.isGrounded){
             velocidadeV = Vector3.down;
         }
         if(Input.GetKey("space") && controller.isGrounded){
             velocidadeV = JumpSpeed * Vector3.up;
+            animator.SetBool("pulando", true);
+        }
+        else if(controller.isGrounded){
+            animator.SetBool("pulando", false);
         }
         velocidadeH = speed * Vector3.right * Input.GetAxis("Horizontal");
-        if(Input.GetAxis("Horizontal") < 0){
-
-        }
         velocidadeV += gravidade * Time.deltaTime * Vector3.down;
         velocidadefinal = velocidadeH + velocidadeV;
         controller.Move(velocidadefinal * Time.deltaTime);
