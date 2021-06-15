c95edbc12fcdfe21f47af3a2cdeaf6e7eededf6d
LuizNery-2 <62023495+LuizNery-2@users.noreply.github.com>
2021-06-15 16:37:21 -0300

    Animação inimigo 1

--- Projetinho/Assets/_scripts/enemyBehavior.cs
+++ Projetinho/Assets/_scripts/enemyBehavior.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,25 +11,41 @@ public class enemyBehavior : MonoBehaviour
     public float speed;
     Vector3 velocidadeV;
     Vector3 velocidadefinal;
-
+    private Vector3 facingRight;
+    private Vector3 facingLeft;
     Vector3 enemy;
     CharacterController controller;
+    private float initialPosition;
     private void Start() {
+        initialPosition = transform.position.x;
         controller = GetComponent<CharacterController>();
         velocidadeV = Vector3.down;
+        facingLeft = transform.localScale;
+        facingRight = transform.localScale;
+        facingLeft.x = facingRight.x * -1;
+        
     }
     void FixedUpdate()
-    {
-        if(player.position.x - transform.position.x >= 0 && player.position.x - transform.position.x <=5){
-        velocidade = speed * Vector3.right * Time.deltaTime;  
-        velocidadefinal = velocidade + velocidadeV; 
-        controller.Move(velocidadefinal);
-        }
-        else if(player.position.x - transform.position.x >= -5 && player.position.x - transform.position.x < 0){
-        velocidade = speed * Vector3.left * Time.deltaTime;
-        velocidadefinal = velocidade + velocidadeV;
+    { MovimentoIni();
+       
+    }
+       private void MovimentoIni()
+    {      velocidade = speed * Vector3.right * Time.deltaTime;
+           velocidadefinal = velocidade + velocidadeV;
             controller.Move(velocidadefinal);
+        
+        if(transform.position.x - initialPosition > 2f){
+            speed *= -1;
+             transform.localScale = facingLeft;
         }
+        else if(transform.position.x - initialPosition < -2f){
+              speed *= -1;
+              transform.localScale = facingRight;
+                 
+                 
+            
         }
-
+       
+        
+    }
 }
