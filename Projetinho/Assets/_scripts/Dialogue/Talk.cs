514de0a47e144986e7d4b72cc799db3a88fe5059
LuizNery-2 <62023495+LuizNery-2@users.noreply.github.com>
2021-09-07 20:26:36 -0300

    Sistema de diálogos

---
+++ Projetinho/Assets/_scripts/Dialogue/Talk.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talk : MonoBehaviour 
+{
+   [SerializeField]
+    DialogueManager dm;
+   [SerializeField]
+    Dialogue[] dialogue;
+   [SerializeField]
+    GameObject playerDialogue;
+    [SerializeField]
+    GameObject NpcDialogue;
+    int contador; 
+
+   
+
+
+      
+
+   private void OnTriggerEnter(Collider other) {
+       
+         
+         
+             if (other.tag == "Player")
+             { 
+                  if(dialogue[contador].GetIsPlayer()) {
+
+                     dm = playerDialogue.GetComponent<DialogueManager>();
+                     dm.SetDialogue(dialogue[contador].GetSpeechText(), dialogue[contador].GetprofileSprite());
+
+                  }
+                  else
+                  { dm = NpcDialogue.GetComponent<DialogueManager>();
+                    dm.SetDialogue(dialogue[contador].GetSpeechText(), dialogue[contador].GetprofileSprite());
+                  }
+                     
+               
+             }
+             
+         
+     
+
+   }
+
+
+   private void OnTriggerExit(Collider other) {
+
+       if (other.tag == "Player")
+     {
+         dm.DisableDialogue();
+     }
+       
+   }
+
+      
+    
+    public void NextSpeaker(){
+             
+             
+             
+             if (dm.GetSpeechText().text == dm.sentences[dm.index])
+            {
+                dm.NextSentence();
+                if(!dm.isSpeaking){
+                  contador++;
+                      if(dialogue[contador].GetIsPlayer()) {
+
+                     dm = playerDialogue.GetComponent<DialogueManager>();
+                     dm.SetDialogue(dialogue[contador].GetSpeechText(), dialogue[contador].GetprofileSprite());
+                     
+
+                  }
+                  else
+                  {
+                    dm = NpcDialogue.GetComponent<DialogueManager>();
+                    dm.SetDialogue(dialogue[contador].GetSpeechText(), dialogue[contador].GetprofileSprite());
+                    
+                  }
+                      
+              
+        
+                }
+
+            } 
+            
+           
+    }
+
+}
